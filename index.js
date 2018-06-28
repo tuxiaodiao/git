0a030f8089e76bc7b1a27c949e3809082e29cfb2
shandaiwang <1364860639@qq.com>
2018-06-28 15:21:23 +0800

    add

--- index.js
+++ index.js
@@ -5,4 +5,8 @@
 mkfile  //创建文件
 git init    //创建git仓库
 git push https://github.com/tuxiaodiao/git.git master   //推送到远程分支
+git config --global user.name shandaiwang   //添加Git显示名称
+git config --global user.email 1364860639@qq.com    //添加Git显示地址
+git remote add git https://github.com/tuxiaodiao/git.git    //设置远程推送别名
+git push git master     //设置远程推送别名后推送
 
